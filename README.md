c101f25c62302ded65988a3135a1895ec5c3c665
collins-ruto <collinsruto48@gmail.com>
2023-11-27 02:06:25 +0300

    tested the methods

--- README.md
+++ README.md
@@ -1,6 +1,90 @@
-# parking_reservation_ICP
+# Parking Management System
 
-Welcome to your first Azle project! This example project will help you to deploy your first canister (application) to the Internet Computer (IC) decentralized cloud. It is a simple getter/setter canister. You can always refer to [The Azle Book](https://demergent-labs.github.io/azle/) for more in-depth documentation.
+## Overview
+
+This project implements a simple parking management system using the Azle programming language. The system allows for the initialization of an owner, addition of parking slots, allocation of parking spaces to clients, vallet delivery, and various operations on parking slots.
+
+## Prerequisites
+
+- Node
+- Typescript
+- DFX
+- IC CDK
+
+## Installation
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/Collins-Ruto/parking-reservation-ICP.git
+    cd parking-reservation-ICP
+    ```
+
+## Project Structure
+
+The project is organized into the following directories and files:
+
+- **`src/`**: Contains the source code for the parking management system.
+  - **`index.ts`**: App entry point Implementation of the parking management system.
+
+- **`node_modules/`**: Directory for project dependencies.
+
+- **`package.json`**: Configuration file for npm, including project dependencies and scripts.
+
+- **`tsconfig.json`**: TypeScript configuration file, specifying compiler options.
+
+- **`LICENSE`**: MIT License file, detailing the terms under which the project is licensed.
+
+- **`README.md`**: Project documentation providing an overview, installation instructions, usage details, and license information.
+
+## Functions
+
+### `initOwner(name: string): string`
+
+- Initializes the system owner with a unique ID, name, and timestamp.
+- There can only be one owner per contract.
+
+### `getOwner(): Owner`
+
+- Retrieves the details of the system owner.
+
+### `getAvailableSlots(): Result<Vec<Parking>, string>`
+
+- Retrieves a list of available parking slots.
+
+### `addParkingSlot(payload: ParkingPayload): string`
+
+- Adds a new parking slot to the system. Reserved for the contract owner.
+
+### `getParkingSpace(payload: AllocationPayload): string`
+
+- Allocates a parking space to a client, marking the slot as occupied.
+
+### `valletDelivery(payload: ValletPayload): string`
+
+- Handles vallet delivery, updating the total cost and client's location.
+
+### `pickupCar(id: string): { msg: string; price: number }`
+
+- Picks up a car from a parking slot, calculates the parking duration and cost.
+
+### `updateParkingSlot(id: string, payload: ParkingPayload): string`
+
+- Updates information for a parking slot, such as parking ID and price. Reserved for the contract owner.
+
+### `deleteParkingSlot(id: string): string`
+
+- Deletes a parking slot from the system. Reserved for the contract owner.
+
+## Usage
+
+- Initialize the owner using `initOwner(name)`.
+- Add parking slots with `addParkingSlot(payload)`.
+- Allocate parking spaces to clients using `getParkingSpace(payload)`.
+- Manage vallet delivery with `valletDelivery(payload)`.
+- Perform various operations on parking slots, including updates and deletions.
+
+## Try it out
 
 `dfx` is the tool you will use to interact with the IC locally and on mainnet. If you don't already have it installed:
 
@@ -30,14 +114,8 @@ npm run canister_deploy_local
 To call the methods on your canister:
 
 ```bash
-npm run canister_call_get_message
-npm run canister_call_set_message
-```
-
-If you run the above commands and then call `npm run canister_call_get_message` you should see:
-
-```bash
-("Hello world!")
+npm run name_of_function
+npm run name_of_function
 ```
 
 Assuming you have [created a cycles wallet](https://internetcomputer.org/docs/current/developer-docs/quickstart/network-quickstart) and funded it with cycles, you can deploy to mainnet like this:
@@ -45,3 +123,7 @@ Assuming you have [created a cycles wallet](https://internetcomputer.org/docs/cu
 ```bash
 npm run canister_deploy_mainnet
 ```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
-import {
-  $query,
-  $update,
-  Record,
-  StableBTreeMap,
-  Vec,
-  match,
-  Result,
-  nat64,
-  ic,
-  Opt,
-  Principal,
-} from "azle";
+// Importing necessary modules from the 'azle' library and 'uuid' library
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, Principal } from 'azle';
 import { v4 as uuidv4 } from "uuid";
 
+// Defining record types for different entities
 type Owner = Record<{
-    id: string;
-    name: string;
-    owner: Principal,
-    created_date: nat64;
-    updated_at: Opt<nat64>;
-}>
+  id: string;
+  name: string;
+  owner: Principal;
+  created_date: nat64;
+  updated_at: Opt<nat64>;
+}>;
 
 type Parking = Record<{
   id: string;
-  parking_id: string;
+  parking_slot: string;
   is_occupied: boolean;
   price: string;
   created_date: nat64;
@@ -47,7 +37,7 @@ type Vallet = Record<{
 }>;
 
 type ParkingPayload = Record<{
-  parking_id: string;
+  parking_slot: string;
   price: string;
 }>;
 
@@ -61,20 +51,27 @@ type ValletPayload = Record<{
   client_location: string;
 }>;
 
+type CarResponse = Record<{
+  msg: string;
+  price: number;
+}>;
+
+// Creating instances of StableBTreeMap for each entity type
 const ownerStorage = new StableBTreeMap<string, Owner>(0, 44, 512);
 const allocationStorage = new StableBTreeMap<string, Allocation>(1, 44, 512);
 const valletStorage = new StableBTreeMap<string, Vallet>(2, 44, 512);
 const parkingStorage = new StableBTreeMap<string, Parking>(3, 44, 512);
 
-$update
+// Initialization of ownerStorage
+$update;
 export function initOwner(name: string): string {
-    if (!ownerStorage.isEmpty()) {
-        return (`Owner has already been initialized`)
-    }
+  if (!ownerStorage.isEmpty()) {
+    return `Owner has already been initialized`;
+  }
   const owner = {
     id: uuidv4(),
-      name: name,
-        owner: ic.caller(),
+    name: name,
+    owner: ic.caller(),
     created_date: ic.time(),
     updated_at: Opt.None,
   };
@@ -82,24 +79,29 @@ export function initOwner(name: string): string {
   return owner.id;
 }
 
-function getOwner(): Owner {
-    return ownerStorage.values()[0]
-}
-
 $query;
+// Function to get available parking slots
 export function getAvailableSlots(): Result<Vec<Parking>, string> {
-    const slots = parkingStorage.values().filter((slot) => !slot.is_occupied);
-    return Result.Ok(slots);
+  const slots = parkingStorage.values().filter((slot) => !slot.is_occupied);
+  if (slots.length == 0) {
+    return Result.Err("No available parking slots currently");
+  }
+  return Result.Ok(slots);
 }
 
 $update;
+// Function to add a new parking slot
 export function addParkingSlot(payload: ParkingPayload): string {
-    if (getOwner().owner !== ic.caller()) {
-        return ("Action reserved for the contract owner");
-    }
+  if (ownerStorage.isEmpty()) {
+    initOwner("cbd parking");
+  }
+  
+  if (isOwner(ic.caller().toText())) {
+    return `Action reserved for the contract owner`;
+  }
   const parking = {
     id: uuidv4(),
-    parking_id: payload.parking_id,
+    parking_slot: payload.parking_slot,
     is_occupied: false,
     price: payload.price,
     created_date: ic.time(),
@@ -109,7 +111,14 @@ export function addParkingSlot(payload: ParkingPayload): string {
   return parking.id;
 }
 
+// Function to check if person making the request is the owner
+function isOwner(caller: string): boolean {
+    const owner = ownerStorage.values()[0];
+    return owner.owner.toText() != caller;
+}
+
 $update;
+// Function to allocate a parking space to a client
 export function getParkingSpace(payload: AllocationPayload): string {
   const allocation = {
     id: uuidv4(),
@@ -129,31 +138,18 @@ export function getParkingSpace(payload: AllocationPayload): string {
     parking.is_occupied = true;
     parkingStorage.insert(parking.id, parking);
   }
-  return `Your Parking ID: ${allocation.id} your Slot: ${allocation.parking_id}`;
-}
-
-$update;
-export function valletDelivery(payload: ValletPayload): string {
-  const price = pickupCar(payload.allocation_id).price;
-  const vallet = {
-    id: uuidv4(),
-    allocation: payload.allocation_id,
-    client_location: payload.client_location,
-    created_date: ic.time(),
-    updated_at: Opt.None,
-  };
-    valletStorage.insert(vallet.id, vallet);
-  return `Your car will be delivered to ${vallet.client_location} new total cost: \$${price + 5}`;
+  return `Your Parking ID: ${allocation.id} your Slot: ${parking.parking_slot}`;
 }
 
 $query;
-export function pickupCar(id: string): { msg: string; price: number } {
+// Function to pick up a car from a parking slot and paying for it
+export function pickupCar(id: string): CarResponse {
   return match(allocationStorage.get(id), {
     Some: (allocation) => {
-      if (allocation.client != ic.caller()) {
+      if (allocation.client.toText() != ic.caller().toText()) {
         return {
-            msg: "You are not the owner of the car in the slot. re-check the slot number",
-            price: 0
+          msg: "You are not the owner of the car in the slot. re-check the slot number",
+          price: 0,
         };
       }
       const parking = match(parkingStorage.get(allocation.parking_id), {
@@ -165,10 +161,10 @@ export function pickupCar(id: string): { msg: string; price: number } {
         parkingStorage.insert(parking.id, parking);
       }
       const time =
-        (Number(allocation.created_date) - Date.now()) / (3600 * 1000);
+        (Number(ic.time()) - Number(allocation.created_date)) / (3600000000000);
       const price = parseInt(parking.price) * time;
       return {
-        msg: `You have parked for : ${time} Hrs. final cost: \$${price}`,
+          msg: `You have parked for : ${time} Hrs. final cost: \$${price}`,
         price: price,
       };
     },
@@ -181,17 +177,35 @@ export function pickupCar(id: string): { msg: string; price: number } {
   });
 }
 
-$update
+$update;
+// Function to handle vallet delivery and add vallet payment
+export function valletDelivery(payload: ValletPayload): string {
+  const price = pickupCar(payload.allocation_id).price;
+  const vallet = {
+    id: uuidv4(),
+    allocation: payload.allocation_id,
+    client_location: payload.client_location,
+    created_date: ic.time(),
+    updated_at: Opt.None,
+  };
+  valletStorage.insert(vallet.id, vallet);
+  return `Your car will be delivered to ${
+    vallet.client_location
+  } new total cost: \$${price + 5}`;
+}
+
+$update;
+// Function to update information for a parking slot
 export function updateParkingSlot(id: string, payload: ParkingPayload): string {
-     if (getOwner().owner !== ic.caller()) {
-        return ("Action reserved for the contract owner");
-    }
+  if (isOwner(ic.caller().toText())) {
+    return "Action reserved for the contract owner";
+  }
   const parking = match(parkingStorage.get(id), {
     Some: (parking) => parking,
     None: () => ({} as unknown as Parking),
   });
   if (parking) {
-    parking.parking_id = payload.parking_id;
+    parking.parking_slot = payload.parking_slot;
     parking.price = payload.price;
     parking.updated_at = Opt.Some(ic.time());
     parkingStorage.insert(parking.id, parking);
@@ -199,24 +213,26 @@ export function updateParkingSlot(id: string, payload: ParkingPayload): string {
   return parking.id;
 }
 
-$update
+$update;
+// Function to delete a parking slot
 export function deleteParkingSlot(id: string): string {
-     if (getOwner().owner !== ic.caller()) {
-        return ("Action reserved for the contract owner");
-    }
+  if (isOwner(ic.caller().toText())) {
+    return "Action reserved for the contract owner";
+  }
   parkingStorage.remove(id);
-  return (`Parking slot of ID: ${id} removed successfully`);
+  return `Parking slot of ID: ${id} removed successfully`;
 }
 
+// Mocking the 'crypto' object for testing purposes
 globalThis.crypto = {
-    // @ts-ignore
-    getRandomValues: () => {
-        let array = new Uint8Array(32);
+  // @ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
 
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256);
-        }
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
 
-        return array;
-    },
+    return array;
+  },
 };
